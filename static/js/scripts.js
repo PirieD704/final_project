6fd78371cad454357851755db5c8b763b08b4197
jttwnsnd <jt.a.townsend@gmail.com>
2016-10-21 16:29:12 -0400

    images, for days

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -41,21 +41,24 @@ The final parameter is an object containing four references to Phasers essential
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'canvas', { preload: preload, create: create, update: update, render:render });
 
+
+var circle, sprite, weapon, cursors, fireButton;
+
 function preload() {
 
-    game.load.image('player', '/static/images/109617.png');
+	game.load.image('background', '/static/images/background.png');
+	game.load.image('particle', '/static/images/green_particle.png')
+    game.load.image('player', '/static/images/player_1.png');
+    game.load.image('shield', '/static/images/line2.png');
 
 }
 
-var sprite;
-var weapon;
-var cursors;
-var fireButton;
-
 function create() {
 
+	game.add.tileSprite(0, 0, 1920, 1920, 'background');
     //  Creates 30 bullets, using the 'bullet' graphic
-    weapon = game.add.weapon(30, 'player');
+    weapon = game.add.weapon(30, 'particle');
+
 
     //  The bullets will be automatically killed when they are 2000ms old
     weapon.bulletKillType = Phaser.Weapon.KILL_LIFESPAN;
@@ -69,12 +72,16 @@ function create() {
 
     //  Wrap bullets around the world bounds to the opposite side
     // weapon.bulletWorldWrap = true;
-
     sprite = this.add.sprite(game.world.centerX, game.world.centerY, 'player');
+    shield = game.add.sprite(game.world.centerX, game.world.centerY, 'shield');
+    sprite.scale.setTo(0.5, 0.5);
+    circleObj = new Phaser.Circle(game.world.centerX, 100,64);
+    circle = game.add.sprite(game.world.centerX, game.world.centerY, circle);
 
     sprite.anchor.set(0.5);
 
     game.physics.arcade.enable(sprite);
+    game.physics.arcade.enable(circle);
 
     sprite.body.drag.set(70);
     sprite.body.maxVelocity.set(200);
@@ -100,17 +107,18 @@ function create() {
 
     //  Enable Arcade Physics for the sprite
     game.physics.enable(sprite, Phaser.Physics.ARCADE);
+    game.physics.enable(circle, Phaser.Physics.ARCADE);
 
     //  Tell it we don't want physics to manage the rotation
     // sprite.body.allowRotation = false;
 
     //camera follows players / center
 
-    // game.add.tileSprite(0, 0, 1920, 1920, 'background');
     game.world.setBounds(0, 0, 1920, 1920);
     game.physics.startSystem(Phaser.Physics.P2JS);
     
     game.physics.p2.enable(sprite);
+    game.physics.p2.enable(circle);
     cursors = game.input.keyboard.createCursorKeys();
     game.camera.follow(sprite);
 
@@ -147,7 +155,8 @@ function update() {
 
     game.world.wrap(sprite, 16);
 
-    sprite.rotation = game.physics.arcade.angleToPointer(sprite);
+    // sprite.rotation = game.physics.arcade.angleToPointer(sprite);
+    sprite.rotation = game.physics.arcade.moveToPointer(sprite, 60, game.input.activePointer, 500);
 
 }
 
@@ -155,5 +164,27 @@ function render() {
 
     // weapon.debug();
     // game.debug.spriteInfo(sprite, 32, 32);
+    game.debug.geom(circle,'#cfffff');
+    // game.debug.text('Diameter : '+circle.diameter,50,200);
+    // game.debug.text('Circumference : '+circle.circumference(),50,230);
 
 }
+
+// var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { create: create, render: render });
+
+// var circle;
+// var floor;
+
+// function create() {
+
+//     circle = new Phaser.Circle(game.world.centerX, 100,64);
+
+// }
+
+// function render () {
+
+//     game.debug.geom(circle,'#cfffff');
+//     game.debug.text('Diameter : '+circle.diameter,50,200);
+//     game.debug.text('Circumference : '+circle.circumference(),50,230);
+
+// }
