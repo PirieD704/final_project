635e7581a0d722c848f4b76dac8f74f0744f4783
PirieD704 <dp75975@gmail.com>
2016-10-21 13:02:22 -0400

    set up routes and node, changed .gitignore and location of node_modules

--- server.js
+++ server.js
@@ -1,1 +1,5 @@
-var express = require('express');
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8000, function(){
+	console.log('Listening on Port 8000...');
+});
--- static/js/mainController.js
+++ static/js/mainController.js
@@ -1,5 +1,21 @@
 var gameApp = angular.module("gameApp", ['ngRoute', 'ngCookies'])
 gameApp.controller('mainController', function($scope, $http, $cookies, $route, $location){
 
-	$scope.test = "IT WORKS!!!!!"
+	
+	$scope.toCanvas = () => {
+		$location.path('/canvas');
+	}
+
+})
+
+gameApp.config(($routeProvider) => {
+	$routeProvider.when('/', {
+		templateUrl: 'views/main.html',
+		controller: 'mainController'
+	}).when('/canvas',{
+		templateUrl: 'views/canvas.html',
+		controller: 'mainController'
+	}).otherwise({
+		redirectTo: '/'
+	})
 })
