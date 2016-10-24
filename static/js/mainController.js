be2fe6d186aed36a267dc824cc745795979b5551
paigen11 <paigen11@gmail.com>
2016-10-24 14:25:34 -0400

    Added login & register functions working

--- static/js/mainController.js
+++ static/js/mainController.js
@@ -1,32 +1,69 @@
 var gameApp = angular.module("gameApp", ['ngRoute', 'ngCookies'])
-gameApp.controller('mainController', function($scope, $http, $cookies, $route, $location){
+gameApp.directive()
+gameApp.controller('mainController', function($scope, $http, $cookies, $route, $location, $rootScope, $timeout){
+
+	var apiPath = 'http://localhost:3000';
 
 	
 	//registration page 
 	$scope.register = function(){
-		console.log($scope.username);
-		//post request sending these 4 variables to the database
-		$http.post(apiPath + '/register', {
+		if($scope.password != $scope.password2){
+			$scope.invalidPass = true;
+			$timeout(function(){
+				$scope.invalidPass = false;
+			}, 1500);
+		}else{
+			//post request sending these 4 variables to the database
+			$http.post(apiPath + '/register', {
+				username: $scope.username,
+				password: $scope.password,
+			}).then(function successCallback(response){
+				console.log(response.data);
+				if(response.data.name == 'nameTaken'){
+					$scope.nameTaken = true;
+					$timeout(function(){
+						$scope.nameTaken = false;
+					}, 1500);
+				}
+				if(response.data.message == 'added'){
+					$scope.welcome = true;
+					$cookies.put('username', $scope.username);
+					$timeout(function(){
+						$location.path('/canvas');
+					}, 1500);
+				}
+			}, function errorCallback(response){
+				console.log(response);
+				
+			})
+		}	
+	};
+
+	//login page
+	$scope.login = function(){
+		$http.post(apiPath + '/login', {
 			username: $scope.username,
-			password: $scope.password,
-			password2: $scope.password2,
-			email: $scope.email
+			password: $scope.password
 		}).then(function successCallback(response){
-			console.log(response.data);
-			if(response.data.name == 'nameTaken'){
-				$scope.nameTaken = true;
-			}
-			if(response.data.message == 'added'){
+			if(response.data.success == 'userFound'){
 				$scope.welcome = true;
-				$rootScope.hi.username = $scope.username;
 				$cookies.put('username', $scope.username);
 				$timeout(function(){
 					$location.path('/canvas');
-				}, 2500);
+				}, 1500);
+			}else if(response.data.failure == 'noUser'){
+				$scope.notFound = true;
+				$timeout(function(){
+					$scope.notFound = false;
+				}, 1500);
+			}else if(response.data.failure == 'badPass'){
+				$scope.badPass = true;
+				$timeout(function(){
+					$scope.badPass = false;
+				}, 1500);
 			}
 		}, function errorCallback(response){
 			console.log(response);
-			
 		})
 	};
 
